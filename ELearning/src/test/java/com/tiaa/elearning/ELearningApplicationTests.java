6ce48040ce2c5f91bc7f4ec16662e06cb8da9bf2
viraj-vvk <viraj.vvk@gmail.com>
2021-06-18 01:00:55 +0530

    Configuration

---
+++ ELearning/src/test/java/com/tiaa/elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiaa.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
