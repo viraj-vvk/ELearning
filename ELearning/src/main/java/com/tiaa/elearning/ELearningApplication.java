224fd894a2bdf10896fa759c3c05bd799ff36b7e
viraj-vvk <viraj.vvk@gmail.com>
2021-06-24 21:41:06 +0530

    Course Management with User Authentication: 1

--- ELearning/src/main/java/com/tiaa/elearning/ELearningApplication.java
+++ ELearning/src/main/java/com/tiaa/elearning/ELearningApplication.java
@@ -1,7 +1,12 @@
 package com.tiaa.elearning;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tiaa.elearning.models.Role;
+import com.tiaa.elearning.repositories.RoleRepository;
 
 @SpringBootApplication
 public class ELearningApplication {
@@ -10,4 +15,31 @@ public class ELearningApplication {
 		SpringApplication.run(ELearningApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+		
+		return args -> {
+			
+			Role role = roleRepository.findByRole("ADMIN");
+			if(role == null) {
+				Role newRole = new Role();
+				newRole.setRole("ADMIN");
+				roleRepository.save(newRole);
+			}
+			
+			role = roleRepository.findByRole("TEACHER");
+			if(role == null) {
+				Role newRole = new Role();
+				newRole.setRole("TEACHER");
+				roleRepository.save(newRole);
+			}
+			
+			role = roleRepository.findByRole("USER");
+			if(role == null) {
+				Role newRole = new Role();
+				newRole.setRole("USER");
+				roleRepository.save(newRole);
+			}
+		};
+	}
 }
